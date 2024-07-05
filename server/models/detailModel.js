e2bbef6f6e440d18e92a69ad6e624b2be1248e46
Struchlak, Joey (ECSA) <Joey.Struchlak@sa.gov.au>
2024-07-05 13:47:31 +0930

    feature/5-Models-details-schema

---
+++ server/models/detailModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema
+// parsing 2 arguments (schema and timestamp)
+const detailSchema = new Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    dob: {
+        type: Date,
+        required: true
+    },
+    aboutMe: {
+        type: String,
+        required: true
+    }
+   /*  profilePic: {
+        type: String,
+        required: true
+    } */
+}, 
+
+
+{ timestamps: true})
+
+// db model as 'Detail' within this model retrieve the detailSchema (schema = structure) (model = model fo the structure)
+module.exports = mongoose.model('Detail', detailSchema)
