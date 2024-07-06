6c290f1c73d71d647daba8bfe9b1a31570355be4
Struchlak, Joey (ECSA) <Joey.Struchlak@sa.gov.au>
2024-07-06 20:39:16 +0930

    feature/7-Create-frontend-React-Application-Pernix-Online-Locker

---
+++ client/src/componenets.js/Navbar.js
@@ -0,0 +1,27 @@
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+
+    return (
+        <header>
+            <div className="container">
+            <Link to="/">
+                    <span>
+                        <h1>
+                            {/* add small custom Pernix image for global nav bar */}
+                            <img
+                                src="/pernix-custom.png"
+                                alt="logo"
+                                style={{ width: '120px', height: '39px', verticalAlign: 'middle', padding: '0 3px', marginTop: '-4px' }}
+                            />
+                            - Online Locker
+                        </h1>
+                        <h3>Your Info, Your Safe Place.</h3>
+                    </span>
+                </Link>
+            </div>
+        </header>
+    )
+}
+
+export default Navbar
