62eec7e91642e74f733b52079ce601abb57b9de9
Struchlak, Joey (ECSA) <Joey.Struchlak@sa.gov.au>
2024-07-23 10:52:21 +0930

    hotfix/deployment-prep

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-
+const cors = require('cors'); 
 const express = require('express')
 const mongoose = require('mongoose')
 const detailRoutes = require('./routes/details')
@@ -9,6 +9,11 @@ const userRoutes = require('./routes/user')
 const app = express()
 
 // middleware
+app.use(cors({
+    origin: 'https://pernixlocker.azurewebsites.net',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    credentials: true 
+}));
 app.use(express.json())
 app.use((req, res, next) => {
     console.log(req.path, req.method)
